43625800f51748d1c12f24ed69b06785810b7b51
growagardenhelp <yhandycar@gmail.com>
2025-08-12 17:27:47 +0800

    Update Egg-EspChanger%.lua

--- Egg-EspChanger%.lua
+++ Egg-EspChanger%.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sigbinhub/pet-changer-beta/refs/heads/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/growagardenhelp/scripth/refs/heads/main/scripth.lua"))()
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/W1rrDg49WNtaclCL/raw", true))()
